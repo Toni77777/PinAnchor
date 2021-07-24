b90a7a986e17051f11f5d1839db26a8f98b03577
Anton Paliakou <>
2021-07-25 01:30:07 +0300

    Add initial PinAnchor.

---
+++ PinAnchor/PinAnchor.h
@@ -0,0 +1,18 @@
+//
+//  PinAnchor.h
+//  PinAnchor
+//
+//  Created by Anton Paliakou on 7/25/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PinAnchor.
+FOUNDATION_EXPORT double PinAnchorVersionNumber;
+
+//! Project version string for PinAnchor.
+FOUNDATION_EXPORT const unsigned char PinAnchorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinAnchor/PublicHeader.h>
+
+
