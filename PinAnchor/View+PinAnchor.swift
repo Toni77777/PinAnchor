b591a080ad8a04cf5f53c0d6fb36ff2db36b4086
Anton Paliakou <>
2021-07-25 01:50:57 +0300

    Refactor return PinView instead UIView.

--- PinAnchor/View+PinAnchor.swift
+++ PinAnchor/View+PinAnchor.swift
@@ -22,19 +22,19 @@
 public extension PinView {
     
     @discardableResult
-    func pinToTop(inset: CGFloat = .zero) -> UIView {
+    func pinToTop(inset: CGFloat = .zero) -> PinView {
         self.topAnchor.constraint(equalTo: self.parent.topAnchor, constant: inset).isActive = true
         return self
     }
     
     @discardableResult
-    func pinToTopSafeArea(inset: CGFloat = .zero) -> UIView {
+    func pinToTopSafeArea(inset: CGFloat = .zero) -> PinView {
         self.topAnchor.constraint(equalTo: self.parent.safeAreaLayoutGuide.topAnchor, constant: inset).isActive = true
         return self
     }
     
     @discardableResult
-    func pinToLeft(inset: CGFloat = .zero) -> UIView {
+    func pinToLeft(inset: CGFloat = .zero) -> PinView {
         self.leadingAnchor.constraint(equalTo: self.parent.leadingAnchor, constant: inset).isActive = true
         return self
     }
@@ -46,37 +46,37 @@ public extension PinView {
     }
     
     @discardableResult
-    func pinToBottom(inset: CGFloat = .zero) -> UIView {
+    func pinToBottom(inset: CGFloat = .zero) -> PinView {
         self.bottomAnchor.constraint(equalTo: self.parent.bottomAnchor, constant: -inset).isActive = true
         return self
     }
     
     @discardableResult
-    func pinToBottomSafeArea(inset: CGFloat = .zero) -> UIView {
+    func pinToBottomSafeArea(inset: CGFloat = .zero) -> PinView {
         self.bottomAnchor.constraint(equalTo: self.parent.safeAreaLayoutGuide.bottomAnchor, constant: inset).isActive = true
         return self
     }
     
     @discardableResult
-    func makeWidth(_ width: CGFloat = .zero) -> UIView {
+    func makeWidth(_ width: CGFloat = .zero) -> PinView {
         self.widthAnchor.constraint(equalToConstant: width).isActive = true
         return self
     }
     
     @discardableResult
-    func makeHeight(_ height: CGFloat = .zero) -> UIView {
+    func makeHeight(_ height: CGFloat = .zero) -> PinView {
         self.heightAnchor.constraint(equalToConstant: height).isActive = true
         return self
     }
     
     @discardableResult
-    func centerVertically(offset: CGFloat = .zero) -> UIView {
+    func centerVertically(offset: CGFloat = .zero) -> PinView {
         self.centerYAnchor.constraint(equalTo: self.parent.centerYAnchor, constant: offset).isActive = true
         return self
     }
     
     @discardableResult
-    func centerHorizontally(offset: CGFloat = .zero) -> UIView {
+    func centerHorizontally(offset: CGFloat = .zero) -> PinView {
         self.centerXAnchor.constraint(equalTo: self.parent.centerXAnchor, constant: offset).isActive = true
         return self
     }
@@ -86,7 +86,7 @@ public extension PinView {
 
 private extension PinView {
     
-    private var parent: UIView {
+    private var parent: PinView {
         guard let parent = self.superview else {
             fatalError("Expected superview but found nil")
         }
